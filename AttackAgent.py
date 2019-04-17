493b8d0fac6cf822f8368cbbb57fa7f5f20b2bdf
Bailey Morgan <bailey.mccarty.morgan@gmail.com>
2019-04-17 14:14:42 -0400

    skelaton classes

---
+++ AttackAgent.py
@@ -0,0 +1,12 @@
+
+
+class AttackAgent:
+
+    def __init__(self):
+        pass
+
+    def deauth(self, target, access_point, packet_count=100):
+        pass
+
+    def crack(self, handshake, wordlist):
+        pass
---
+++ NetworkController.py
@@ -0,0 +1,22 @@
+import os, subprocess, scapy
+
+
+class NetworkController:
+
+    def __init__(self):
+        pass
+
+    def get_access_point(self):
+        pass
+
+    def get_stations(self):
+        pass
+
+    def start_scanning(self):
+        pass
+
+    def stop_scanning(self):
+        pass
+
+    def capture_handshake(self, access_point):
+        pass
