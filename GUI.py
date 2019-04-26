f499b0986a54c0463a9f6f387e377dc8c5393bfb
Jacob Petrisko <petrisjj@rose-hulman.edu>
2019-04-26 14:46:20 -0400

    Modified the GUI so it can be used as an object in a main method somewhere

--- GUI.py
+++ GUI.py
@@ -3,10 +3,13 @@ from tkinter.ttk import *
 from NetworkController import NetworkController
 from AttackAgent import AttackAgent
 import time
-import _thread
+import threading
 
 
 class GUI:
+
+    threads = []
+    scanCounter = 0;
 
     def __init__(window, networkController):
         self.window = window
@@ -16,15 +19,21 @@ class GUI:
     def configure_window():
         self.window.title("free-whify | Wifi Deauther and Cracker")
         self.window.geometry('500x500')
-        startScanning = Button(self.window, text="Start Scanning", command= lambda: (start()))
-        stopScanning = Button(self.window, text="Stop Scanning", command= lambda: (stop()))
+        startScanning = Button(self.window, text="Start Scanning", command= lambda: (startStopScanning(True)))
+        stopScanning = Button(self.window, text="Stop Scanning", command= lambda: (startStopScanning(False)))
 
         startScanning.grid(column=0, row=0)
         stopScanning.grid(column=3, row=0)
         self.window.mainloop()
 
-    def start():
-        _thread.start_new_thread(self.networkController.channel_hopper, ())
-
-    def stop():
-        nc.stop_scanning()
+    def startStopScanning(scan):
+        if(scan and scanCounter == 0):
+            scanCounter++
+            scanThread = threading.thread(target = self.networkController.channel_hopper, ())
+            threads.append(scanThread)
+            scanThread.start()
+        else if(!scan and scanCounter == 1):
+            scanCounter--
+            threads[0].join()
+        else:
+            #There is no scan to stop or one is already started
